d732b4b3cd45b2953294d822420eb683dd6cac42
Neha <28neha@gmail.com>
2016-09-02 04:01:45 -0700

    timeline with logos

--- script.js
+++ script.js
@@ -44,13 +44,13 @@ function($scope,$location, $anchorScroll){
       $anchorScroll();
   }
   
- $scope.timelineEvents=[{Event:'Graduating soon',Desc:'bah bah',On:'April-2017'},
- {Event:'Teaching Assistant',Desc:'bah bah',On:'August-2016'},
- {Event:'Joined UCSD',Desc:'ehi',On:'Sept-2015'},
- {Event:'Software Engineer - 1',Desc:'Promoted to Software Engineer 1. We moved to .Net MVC and got opprotunity to work on more challenging projects.',On:'July-1-2014'},
- {Event:'Trainee Software Engineer',Desc:'Joined Microchip Technology in Bangalore as Trainee Software Engineer. I joined an amazing team at MCHP - internet and intranet application team. We used .Net framework 3.5 c# webform application, Sql mainly. I was an integral part of microchips e-commerce website microchipdirect.com.',On:'July-1-2013'},
- {Event:'Bachelors in Computer Science',Desc:'Graduated from MSRIT with the degree in Bachelors of Computer Science.',On:'June-2013'},
- {Event:'Joined MSRIT',Desc:'I joined M.S. Ramaiah Institute of Technology, Bangalore, India to pursue bachelors in Computer Science. MSRIT is one the top five college in the state of karnataka. Checkout wiki for more information.',On:'Sept-09-2009'}
+ $scope.timelineEvents=[{Event:'Graduating soon',Desc:'bah bah',On:'April-2017',Pic:'http://icons.iconarchive.com/icons/icons8/windows-8/256/Business-Graduation-Cap-icon.png'},
+ {Event:'Teaching Assistant',Desc:'bah bah',On:'August-2016',Pic:'http://www.sysnet.ucsd.edu/~voelker/pubcom/logo/CSELogo_RGB.gif'},
+ {Event:'Joined UCSD',Desc:'ehi',On:'Sept-2015',Pic:'http://www.universityreview.org/wp-content/uploads/2012/03/University-of-California-San-Diego-Seal.png'},
+ {Event:'Software Engineer - 1',Desc:'Promoted to Software Engineer 1. We moved to .Net MVC and got opprotunity to work on more challenging projects.',On:'July-1-2014',Pic:'http://www.dcdcselector.com/media/Microchipdirect-Logo-s_356_l.png'},
+ {Event:'Trainee Software Engineer',Desc:'Joined Microchip Technology in Bangalore as Trainee Software Engineer. I joined an amazing team at MCHP - internet and intranet application team. We used .Net framework 3.5 c# webform application, Sql mainly. I was an integral part of microchips e-commerce website microchipdirect.com.',On:'July-1-2013',Pic:'https://upload.wikimedia.org/wikipedia/en/9/92/MicrochipTechnology_Logo.png'},
+ {Event:'Bachelors in Computer Science',Desc:'Graduated from MSRIT with the degree in Bachelors of Computer Science.',On:'June-2013',Pic:'http://icons.iconarchive.com/icons/icons8/windows-8/256/Business-Graduation-Cap-icon.png'},
+ {Event:'Joined MSRIT',Desc:'I joined M.S. Ramaiah Institute of Technology, Bangalore, India to pursue bachelors in Computer Science. MSRIT is one the top five college in the state of karnataka. Checkout wiki for more information.',On:'Sept-09-2009',Pic:'https://upload.wikimedia.org/wikipedia/en/b/b8/MSRIT_Golden_Jubilee_Logo.png'}
  ];
  
  $scope.yAxisLabels = ['Uh? Next Question','Basic','Medium','Pretty Good','Master'];
